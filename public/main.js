f7c6d69bce32070050460e4c03c71177ae5b34a5
Philip Krause <philipkrause7@gmail.com>
2019-05-17 09:42:45 -0400

    layoutComplete,fontawesomeNext

--- public/main.js
+++ public/main.js
@@ -1,7 +1,35 @@
+const get_pic_api = () => {
+  console.log('fetching api')
+
+  fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data.url)
+      document.querySelector(
+        '.image-of-the-day'
+      ).style.backgroundImage = `url(${data.url})`
+    })
+}
+
+const get_news_api = () => {
+  console.log('fetching api 2')
+
+  fetch('https://sdg-astro-api.herokuapp.com/api/SpaceX/launches/upcoming')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data[0].mission_name)
+      document.querySelector('.news-feed').appendChild('h2').textContent =
+        data[0].mission_name
+    })
+}
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  get_pic_api()
+  get_news_api()
 }
 
 document.addEventListener('DOMContentLoaded', main)
