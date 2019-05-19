964c83644093c83be62174774b27b5ce7499bb4c
Philip Krause <philipkrause7@gmail.com>
2019-05-19 17:08:07 -0400

    fontsh*tANDScroll,TimerNext

--- public/main.js
+++ public/main.js
@@ -1,3 +1,6 @@
+let slide = 0
+const allData = []
+
 const get_pic_api = () => {
   console.log('fetching api')
 
@@ -12,7 +15,6 @@ const get_pic_api = () => {
       ).style.backgroundImage = `url(${data.url})`
     })
 }
-
 const get_news_api = () => {
   console.log('fetching api 2')
 
@@ -21,15 +23,28 @@ const get_news_api = () => {
       return response.json()
     })
     .then(data => {
-      console.log(data[0].mission_name)
-      document.querySelector('.news-feed').appendChild('h2').textContent =
-        data[0].mission_name
+      console.log(slide)
+      let allData = data
+      document.querySelector('.news-feed h2 span').textContent =
+        data[slide].mission_name
+      document.getElementById('details').textContent = data[slide].details
+      document.getElementById('eta').textContent = data[slide].launch_date_local
+      document.getElementById('launch_name').textContent =
+        data[slide].launch_site.site_name_long
     })
 }
-
+const nextSlide = () => {
+  slide += 1
+  get_news_api()
+}
+const prevSlide = () => {
+  slide -= 1
+  get_news_api()
+}
 const main = () => {
   get_pic_api()
   get_news_api()
 }
-
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.right-button').addEventListener('click', nextSlide)
+document.querySelector('.left-button').addEventListener('click', prevSlide)
